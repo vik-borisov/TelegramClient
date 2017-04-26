e2300986a3f987a1008beb561fdb555989c503bc
Виктор Борисов <vik.borisov@outlook.com>
2017-04-26 12:08:14 +0700

    TelegramClient.UnitTests project was added

---
+++ tests/TelegramClient.UnitTests/TcpTransportTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramClient.UnitTests
+{
+    using Xunit;
+
+    public class TcpTransportTests
+    {
+        [Fact]
+        public void EnsureClientConnected()
+        {
+            
+        }
+    }
+}
