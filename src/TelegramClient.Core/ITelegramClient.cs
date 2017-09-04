dacf9344974cb1c3c4aa41119a8b56d77216c8f0
Viktor Borisov <vik.borisov@outlook.com>
2017-09-04 22:42:36 +0700

    Add comments for the interface ITelegramClient

--- src/TelegramClient.Core/ITelegramClient.cs
+++ src/TelegramClient.Core/ITelegramClient.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
+
 [assembly: InternalsVisibleTo("TelegramClient.UnitTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 
@@ -8,18 +9,25 @@ namespace TelegramClient.Core
 
     public interface ITelegramClient
     {
+        /// <summary>Send custom messages</summary>
         ISenderService SendService { get; }
 
+        /// <summary>Automatic and manual updates</summary>
         IUpdatesApiService UpdatesService { get; }
 
+        /// <summary>Connecting to the server</summary>
         IConnectApiService ConnectService { get; }
 
+        /// <summary>Registration and authentication</summary>
         IAuthApiService AuthService { get; }
 
+        /// <summary>Messages and chats</summary>
         IMessagesApiService MessagesService { get; }
 
+        /// <summary>Working with contacts</summary>
         IContactsApiService ContactsService { get; }
 
+        /// <summary>Working with files</summary>
         IUploadApiService UploadService { get; }
     }
 }
