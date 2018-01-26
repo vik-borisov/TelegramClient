99c3f56ab9dcedb62c4bafac92ab5b2298b538a7
Iurii Caisin <iurii.caisin@arvato.com>
2018-01-26 10:30:11 -0800

    added editorconfig to comply with project stylign guidelines

--- src/TelegramClient.Core/Helpers/TcpClientHelper.cs
+++ src/TelegramClient.Core/Helpers/TcpClientHelper.cs
@@ -2,25 +2,25 @@
 
 namespace TelegramClient.Core.Helpers
 {
-	public static class TcpClientHelper
-	{
-		public static bool IsConnected(this TcpClient client)
-		{
-			if (client == null || !client.Connected || client.Client == null || !client.Client.Connected)
-			{
-				return false;
-			}
+    public static class TcpClientHelper
+    {
+        public static bool IsConnected(this TcpClient client)
+        {
+            if (client == null || !client.Connected || client.Client == null || !client.Client.Connected)
+            {
+                return false;
+            }
 
-			if (client.Client.Poll(0, SelectMode.SelectRead))
-			{
-				byte[] buff = new byte[1];
-				if (client.Client.Receive(buff, SocketFlags.Peek) == 0)
-				{
-					return false;
-				}
-			}
-			return true;
-		}
+            if (client.Client.Poll(0, SelectMode.SelectRead))
+            {
+                byte[] buff = new byte[1];
+                if (client.Client.Receive(buff, SocketFlags.Peek) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
-	}
+    }
 }
--- src/TelegramClient.Core/Network/Tcp/TcpService.cs
+++ src/TelegramClient.Core/Network/Tcp/TcpService.cs
@@ -1,94 +1,94 @@
 ﻿namespace TelegramClient.Core.Network.Tcp
 {
-	using System;
-	using System.IO;
-	using System.Net;
-	using System.Net.Sockets;
-	using System.Threading;
-	using System.Threading.Tasks;
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading;
+    using System.Threading.Tasks;
 
-	using TelegramClient.Core.IoC;
-	using TelegramClient.Core.Settings;
-	using TelegramClient.Core.Helpers;
+    using TelegramClient.Core.IoC;
+    using TelegramClient.Core.Settings;
+    using TelegramClient.Core.Helpers;
 
-	[SingleInstance(typeof(ITcpService))]
-	internal class TcpService : ITcpService
-	{
-		private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+    [SingleInstance(typeof(ITcpService))]
+    internal class TcpService : ITcpService
+    {
+        private readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
 
-		private TcpClient _tcpClient;
+        private TcpClient _tcpClient;
 
-		public IClientSettings ClientSettings { get; set; }
+        public IClientSettings ClientSettings { get; set; }
 
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
-		public async Task<Stream> Receieve()
-		{
-			await EnsureClientConnected().ConfigureAwait(false); 
-			return _tcpClient.GetStream();
-		}
+        public async Task<Stream> Receieve()
+        {
+            await EnsureClientConnected().ConfigureAwait(false);
+            return _tcpClient.GetStream();
+        }
 
-		public async Task Send(byte[] encodedMessage)
-		{
-			await EnsureClientConnected().ConfigureAwait(false);
-			await _tcpClient.GetStream().WriteAsync(encodedMessage, 0, encodedMessage.Length).ConfigureAwait(false);
-		}
+        public async Task Send(byte[] encodedMessage)
+        {
+            await EnsureClientConnected().ConfigureAwait(false);
+            await _tcpClient.GetStream().WriteAsync(encodedMessage, 0, encodedMessage.Length).ConfigureAwait(false);
+        }
 
-		private void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-				_semaphore?.Dispose();
-				_tcpClient?.Dispose();
-			}
-		}
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _semaphore?.Dispose();
+                _tcpClient?.Dispose();
+            }
+        }
 
-		private async Task Reconnect()
-		{
-			var session = ClientSettings.Session;
-			_tcpClient = new TcpClient();
-			await _tcpClient.ConnectAsync(session.ServerAddress, session.Port).ConfigureAwait(false);
-		}
+        private async Task Reconnect()
+        {
+            var session = ClientSettings.Session;
+            _tcpClient = new TcpClient();
+            await _tcpClient.ConnectAsync(session.ServerAddress, session.Port).ConfigureAwait(false);
+        }
 
-		private async Task EnsureClientConnected()
-		{
-			if (_tcpClient == null)
-			{
-				await _semaphore.WaitAsync().ConfigureAwait(false);
-				if (_tcpClient == null)
-				{
-					await this.Reconnect().ConfigureAwait(false);
-				}
-				_semaphore.Release();
-			}
-			else
-			{
-				if (!_tcpClient.IsConnected())
-				{
-					await _semaphore.WaitAsync();
-					var endpoint = (IPEndPoint)_tcpClient.Client.RemoteEndPoint;
-					var session = ClientSettings.Session;
+        private async Task EnsureClientConnected()
+        {
+            if (_tcpClient == null)
+            {
+                await _semaphore.WaitAsync().ConfigureAwait(false);
+                if (_tcpClient == null)
+                {
+                    await this.Reconnect().ConfigureAwait(false);
+                }
+                _semaphore.Release();
+            }
+            else
+            {
+                if (!_tcpClient.IsConnected())
+                {
+                    await _semaphore.WaitAsync();
+                    var endpoint = (IPEndPoint)_tcpClient.Client.RemoteEndPoint;
+                    var session = ClientSettings.Session;
 
-					if (!_tcpClient.IsConnected() || endpoint.Address.ToString() != session.ServerAddress || endpoint.Port != session.Port)
-					{
-						_tcpClient.Dispose();
-						_tcpClient = null;
+                    if (!_tcpClient.IsConnected() || endpoint.Address.ToString() != session.ServerAddress || endpoint.Port != session.Port)
+                    {
+                        _tcpClient.Dispose();
+                        _tcpClient = null;
 
-						await this.Reconnect().ConfigureAwait(false);
-					}
+                        await this.Reconnect().ConfigureAwait(false);
+                    }
 
-					_semaphore.Release();
-				}
-			}
-		}
+                    _semaphore.Release();
+                }
+            }
+        }
 
-		~TcpService()
-		{
-			Dispose(false);
-		}
-	}
+        ~TcpService()
+        {
+            Dispose(false);
+        }
+    }
 }
